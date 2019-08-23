d819fd1c2503f62b88eacf0c554abc587a2d2c5a
Kai Nuoranen <kai.nuoranen@gmail.com>
2019-08-23 11:12:18 +0300

    Pyorii jo Netlifyssa, paivitetty oikeiksi backendin heroku-osoite kaikissa fetch-kohdissa. Toivottavasti muutokset paivittyy myos Netlifyhyn

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://protected-shore-56708.herokuapp.com/', {
+      fetch('https://facerecognitionbackendleso110.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://protected-shore-56708.herokuapp.com/image', {
+          fetch('https://facerecognitionbackendleso110.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://protected-shore-56708.herokuapp.com/signin', {
+    fetch('https://facerecognitionbackendleso110.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
